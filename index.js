14f6c7cc59af8a94830eedabe6c57eed91ccbed6
vbrischk <brischkofski@gmail.com>
2021-09-27 15:05:53 +0200

    teest

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hi from console")
+const jquery = require('jquery')
+//
+$ = window.$ = window.jQuery = jquery;
+
+$(function () {
+    let endpoint = 'https://jsonplaceholder.typicode.com/todos/1'
+    $.ajax({
+        url: endpoint,
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (result) {
+            console.log(result)
+            var id = document.getElementById("todoID")
+            var title = document.getElementById("todoTitle")
+            var isCompleted = document.getElementById("isC")
+
+            id.innerHTML = 'ID' + result.userId
+            title.innerHTML = 'Title:' + result.title
+            isCompleted.innerHTML = 'is Completed: ' + result.completed
+        }
+    })
+
+})
